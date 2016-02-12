9ae8721b3b4e9917388be44e549cd83cbf8224c9
Shih-Yuan Liu <syliu@mit.edu>
2016-02-11 22:17:33 -0500

    Add videos download script to videos folder.

---
+++ videos/download_videos.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+wget -O lane_following_1.mp4 https://www.dropbox.com/s/bzom8ice6t0uy0p/20160208_on_board_lane_following_1.mp4?dl=0
+wget -O lane_following_2.mp4 https://www.dropbox.com/s/oe4o2ybbf1jpcch/20160208_on_board_lane_following_2.mp4?dl=0
