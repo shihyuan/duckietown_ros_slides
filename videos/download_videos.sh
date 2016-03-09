b12d256d10addf587a6f9fff9bc97ee1e239f5a0
Shih-Yuan Liu <syliu@mit.edu>
2016-03-08 23:31:38 -0500

    Update download vidoes script.

--- videos/download_videos.sh
+++ videos/download_videos.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
-wget -O lane_following_1.mp4 https://www.dropbox.com/s/bzom8ice6t0uy0p/20160208_on_board_lane_following_1.mp4?dl=0
-wget -O lane_following_2.mp4 https://www.dropbox.com/s/oe4o2ybbf1jpcch/20160208_on_board_lane_following_2.mp4?dl=0
+wget -O ground_projection.mp4 https://www.dropbox.com/s/wzh71ey4ryj3j6y/ground_projection.mp4?dl=0
+wget -O lane_control_IO.mp4 https://www.dropbox.com/s/6utakdpq354e219/lane_control_IO.mp4?dl=0
+wget -O lane_control.mp4 https://www.dropbox.com/s/ms2m6us3xw8fq5k/lane_control.mp4?dl=0
+wget -O lane_following_1.mp4 https://www.dropbox.com/s/smlnia30aax2v9u/lane_following_1.mp4?dl=0
+wget -O lane_following_2.mp4 https://www.dropbox.com/s/4uvv2d3q4xf3ae9/lane_following_2.mp4?dl=0
+wget -O lane_following_loop.mp4 https://www.dropbox.com/s/z4mm2q7bfcc3d1q/lane_following_loop.mp4?dl=0
+wget -O lane_following_all.mp4 https://www.dropbox.com/s/ifefw12q3wpoxzp/lane_following_all.mp4?dl=0
+wget -O lane_filter.mp4 https://www.dropbox.com/s/tp27tbu9rvbk0i1/lane_filter.mp4?dl=0
+wget -O line_detection.mp4 https://www.dropbox.com/s/qne0l7enh1z18ij/line_detection.mp4?dl=0
